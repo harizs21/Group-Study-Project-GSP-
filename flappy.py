5310a698ea0637dbe08b77fad39534dc67f4f68a
navidm33 <navidm33@kprschools.ca>
2024-01-17 09:53:20 -0500

    test git push

--- flappy.py
+++ flappy.py
@@ -1,24 +1,21 @@
-import pygame, random, time, sys, os
-from pygame.locals import *
+
 import pygame
+import random
+import time
 from pygame.locals import *
 from pygame import mixer
-
 
 screen_width = 400
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Flappy Bird")
 
-
 mixer.init()
 mixer.music.load('assets/audio/bensound-summer_ogg_music.ogg')
 mixer.music.play()
 
-
 pygame.init()
 pygame.font.init()
-
 
 # VARIABLES
 SCREEN_WIDHT = 400
@@ -27,7 +24,7 @@ SPEED = 20
 GRAVITY = 2.5
 GAME_SPEED = 15
 score = 0
-score_display = pygame.font.Font('freesansbold.ttf')
+display_score = pygame.font.Font('freesansbold.ttf', 36)
 pass_pipe = False
 GROUND_WIDHT = 2 * SCREEN_WIDHT
 GROUND_HEIGHT = 100
@@ -43,9 +40,7 @@ hit = 'assets/audio/hit.wav'
 pygame.mixer.init()
 
 
-
 class Bird(pygame.sprite.Sprite):
-
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
@@ -54,24 +49,17 @@ class Bird(pygame.sprite.Sprite):
                        pygame.image.load('assets/sprites/yellowbird-downflap.png').convert_alpha()]
 
         self.speed = SPEED
-
-
-
         self.current_image = 0
         self.image = pygame.image.load('assets/sprites/bluebird-upflap.png').convert_alpha()
         self.mask = pygame.mask.from_surface(self.image)
-
         self.rect = self.image.get_rect()
         self.rect[0] = SCREEN_WIDHT / 6
         self.rect[1] = SCREEN_HEIGHT / 2
-
 
     def update(self):
         self.current_image = (self.current_image + 1) % 3
         self.image = self.images[self.current_image]
         self.speed += GRAVITY
-
-        # UPDATE HEIGHT
         self.rect[1] += self.speed
 
     def bump(self):
@@ -83,13 +71,10 @@ class Bird(pygame.sprite.Sprite):
 
 
 class Pipe(pygame.sprite.Sprite):
-
     def __init__(self, inverted, xpos, ysize):
         pygame.sprite.Sprite.__init__(self)
-
         self.image = pygame.image.load('assets/sprites/img_13.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (PIPE_WIDHT, PIPE_HEIGHT))
-
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
 
@@ -102,34 +87,27 @@ class Pipe(pygame.sprite.Sprite):
             self.direction = -1
 
         self.mask = pygame.mask.from_surface(self.image)
-
-        self.amplitude = -5 #speed
-        self.frequency = 3.0 #up and down
+        self.amplitude = -5  # speed
+        self.frequency = 3.0  # up and down
         self.time = 2
 
     def update(self):
         self.rect[0] -= GAME_SPEED
-
-        # Update the y-coordinate based on the direction of motion
         self.rect[1] += self.direction * self.amplitude * pygame.math.Vector2(0, 1).rotate(self.frequency * self.time).y
         self.time += 1
 
-        # Change the direction when the pipe reaches the top or bottom of the screen
         if self.direction == -1 and self.rect.top < 0:
-            self.direction = 1  # Change to downward motion
+            self.direction = 1
         elif self.direction == 1 and self.rect.bottom > SCREEN_HEIGHT:
-            self.direction = -1  # speed
+            self.direction = -1
 
 
 class Ground(pygame.sprite.Sprite):
-
     def __init__(self, xpos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('assets/sprites/img_5.png').convert_alpha()
         self.image = pygame.transform.scale(self.image, (GROUND_WIDHT, GROUND_HEIGHT))
-
         self.mask = pygame.mask.from_surface(self.image)
-
         self.rect = self.image.get_rect()
         self.rect[0] = xpos
         self.rect[1] = SCREEN_HEIGHT - GROUND_HEIGHT
@@ -146,17 +124,10 @@ def get_random_pipes(xpos):
     size = random.randint(0, 300)
     pipe = Pipe(False, xpos, size)
     pipe_inverted = Pipe(True, xpos, SCREEN_HEIGHT - size - PIPE_GAP)
-
-    # Define the scoring rectangle
     score_rectangle = pygame.Rect(xpos + PIPE_WIDHT // 2 - 5, 0, 10, SCREEN_HEIGHT)
-
     return pipe, pipe_inverted, score_rectangle
 
 
-
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
-pygame.display.set_caption('Flappy Bird')
 
 BACKGROUND = pygame.image.load('assets/sprites/underwater11.jpg')
 BACKGROUND = pygame.transform.scale(BACKGROUND, (SCREEN_WIDHT, SCREEN_HEIGHT))
@@ -183,9 +154,7 @@ clock = pygame.time.Clock()
 begin = True
 
 while begin:
-
     clock.tick(15)
-
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -194,6 +163,7 @@ while begin:
                 bird.bump()
                 pygame.mixer.music.load(wing)
                 pygame.mixer.music.play()
+
                 begin = False
 
     screen.blit(BACKGROUND, (0, 0))
@@ -201,7 +171,6 @@ while begin:
 
     if is_off_screen(ground_group.sprites()[0]):
         ground_group.remove(ground_group.sprites()[0])
-
         new_ground = Ground(GROUND_WIDHT - 20)
         ground_group.add(new_ground)
 
@@ -211,10 +180,12 @@ while begin:
     bird_group.draw(screen)
     ground_group.draw(screen)
 
+    score_text = display_score.render(f"Score: {score}", True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
     pygame.display.update()
 
 while True:
-
     clock.tick(15)
 
     for event in pygame.event.get():
@@ -251,6 +222,10 @@ while True:
     pipe_group.draw(screen)
     ground_group.draw(screen)
 
+    score += 1  # Update the score
+    score_text = display_score.render(f"Score: {score}", True, (255, 255, 255))
+    screen.blit(score_text, (10, 10))
+
     pygame.display.update()
 
     if (pygame.sprite.groupcollide(bird_group, ground_group, False, False,   pygame.sprite.collide_mask) or
@@ -259,10 +234,14 @@ while True:
         pygame.mixer.music.play()
         time.sleep(1)
         break
-score_display(score)
+
+display_score(score)
 screen = pygame.display.set_mode((750, 450))
 clock = pygame.time.Clock()
 player = pygame.Rect(100, 200, 50, 50)
 obstacle = pygame.Rect(200, 200, 50, 50)
 while True:
     font = pygame.font.Font(None, 36)
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDHT, SCREEN_HEIGHT))
+pygame.display.set_caption('Flappy Bird')
